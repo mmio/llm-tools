e41bad2514b88d56cf57f27fd3da028eb40f2afe
Dominik Vasko <dominikvasko96@gmail.com>
2026-01-22 17:59:06 +0100

    test file

---
+++ test.py
@@ -0,0 +1,11 @@
+# import os
+# import pathlib
+#
+# cwd = os.getcwd()
+# path = pathlib.Path(cwd)
+#
+# print(path)
+
+from agents.instructiongenerator import InstructionGenerator
+
+igen = InstructionGenerator()
